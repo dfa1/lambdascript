2be387c61d6a9755f2407dbd9772fa4767cba0c7
Davide Angelocola <davide.angelocola@gmail.com>
2009-12-12 17:07:25 +0100

    running tests for javascript 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7

--- src/test/java/com/humaorie/lambdascript/LambdaScriptTest.java
+++ src/test/java/com/humaorie/lambdascript/LambdaScriptTest.java
@@ -9,14 +9,54 @@ import org.mozilla.javascript.Scriptable;
 // TODO: write a custom runner
 public class LambdaScriptTest {
 
-    @Test
-    public void test() throws IOException {
+    public void runSuite(int javascriptVersion) throws IOException {
         Context context = Context.enter();
+        context.setLanguageVersion(javascriptVersion);
         Scriptable scope = context.initStandardObjects();
         context.evaluateReader(scope, new FileReader("src/main/javascript/functional.js"), "functional.js", 1, null);
         context.evaluateString(scope, "LambdaScript.install();", "string", 1, null);
         context.evaluateReader(scope, new FileReader("src/test/java/com/humaorie/lambdascript/test.js"), "test.js", 1, null);
         context.evaluateReader(scope, new FileReader("src/test/java/com/humaorie/lambdascript/timer.js"), "timer.js", 1, null);
         context.evaluateReader(scope, new FileReader("src/test/javascript/functionalTest.js"), "", 1, null);
+    }
+
+    @Test
+    public void javascript10() throws IOException {
+        runSuite(Context.VERSION_1_0);
+    }
+
+    @Test
+    public void javascript11() throws IOException {
+        runSuite(Context.VERSION_1_1);
+    }
+
+    @Test
+    public void javascript12() throws IOException {
+        runSuite(Context.VERSION_1_2);
+    }
+
+    @Test
+    public void javascript13() throws IOException {
+        runSuite(Context.VERSION_1_3);
+    }
+
+    @Test
+    public void javascript14() throws IOException {
+        runSuite(Context.VERSION_1_4);
+    }
+
+    @Test
+    public void javascript15() throws IOException {
+        runSuite(Context.VERSION_1_5);
+    }
+
+    @Test
+    public void javascript16() throws IOException {
+        runSuite(Context.VERSION_1_6);
+    }
+
+    @Test
+    public void javascript17() throws IOException {
+        runSuite(Context.VERSION_1_7);
     }
 }
