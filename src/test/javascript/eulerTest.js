8f187346e31a87eb784a44d48bcb4b9a649cc013
Davide Angelocola <davide.angelocola@gmail.com>
2009-12-15 10:47:04 +0100

    cosmetics

--- src/test/javascript/eulerTest.js
+++ src/test/javascript/eulerTest.js
@@ -1,5 +1,6 @@
 var suite = new Suite('Project Euler');
 
+// utils
 function println(object) {
     if (typeof object != 'string') {
         object = object.toString();
@@ -16,6 +17,7 @@ function print(object) {
     java.lang.System.out.print(object);
 }
 
+// problem 1
 function sum(array) {
     return reduce(lambda('a+b'), array, 0);
 }
@@ -33,6 +35,24 @@ suite.testProblem1 = function() {
     Assert.that(problem1(999), Matcher.is(233168));
 }
 
+// problem 2
+function fib(n) {
+    if (n == 1 || n == 2) {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+suite.testFib = function() {
+    Assert.that(map(fib, range(10)), Matcher.array([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]));
+}
+
+suite.testProblem2 = function() {
+    each(compose(println, fib), range(15));
+}
+
+// problem 5
 function smallestIntegerDividedBy(factors) {
     return reduce(lambda('a*b'), factors, 1);
 }
@@ -44,6 +64,7 @@ function problem5(n) {
 suite.testProblem5 = function() {
     }
 
+// problem 6: naive version
 function sumOfSquares(n) {
     return sum(map(lambda('a*a'), range(n)));
 }
@@ -61,20 +82,5 @@ suite.testProblem6 = function() {
     Assert.that(problem6(100), Matcher.is(25164150));
 }
 
-function fib(n) {
-    if (n == 1 || n == 2) {
-        return n;
-    } else {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
-
-suite.testFib = function() {
-    Assert.that(map(fib, range(10)), Matcher.array([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]));
-}
-
-suite.testProblem2 = function() {
-
-}
-
+// run all tests
 suite.run();
