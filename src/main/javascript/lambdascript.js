6b897735ccafb46abcb0ed5c4edb52167021fa7e
Davide Angelocola <davide.angelocola@gmail.com>
2010-02-06 13:26:24 +0100

    added LGPL headers

--- src/main/javascript/lambdascript.js
+++ src/main/javascript/lambdascript.js
@@ -1,12 +1,11 @@
 /*
- * LambdaScript ${pom.version} (${changeSet} on ${changeSetDate})
- * (c) 2009 Davide Angelocola <davide.angelocola@gmail.com>
- * http://bitbucket.org/dfa/lambdascript
- * 
+ * LambdaScript, http://bitbucket.org/dfa/lambdascript
+ * (c) 2009, 2010 Davide Angelocola <davide.angelocola@gmail.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +13,7 @@
  * GNU Lesser Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
